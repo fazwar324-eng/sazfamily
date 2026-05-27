f45b57d3b60c3ec45e3b82435fb59b8ce4cc1604
fazwar324-eng <fazwar324@gmail.com>
2026-05-27 19:46:12 +0700

    Refactor music controls and add toggle function
    Removed music button event listener and added toggleMusic function.

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
+const music = document.getElementById('bg-music');
 
-const musicBtn = document.getElementById('musicBtn');
-const music = document.getElementById('bgMusic');
-
-let isPlaying = false;
-
-musicBtn.addEventListener('click', () => {
-if(!isPlaying){
-music.play();
-musicBtn.innerText='⏸ Pause Music';
-isPlaying=true;
-}else{
-music.pause();
-musicBtn.innerText='🎵 Play Music';
-isPlaying=false;
-}
-});
-
-function copyPrompt(button){
-const text = button.parentElement.querySelector('p').innerText;
-navigator.clipboard.writeText(text);
-button.innerText='Copied!';
-setTimeout(()=>{
-button.innerText='Copy Prompt';
-},2000);
+function toggleMusic() {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
 }
