376a32b3975582acb94d3ad11280ec8a06e687fd
fazwar324-eng <fazwar324@gmail.com>
2026-05-28 22:54:08 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const music = document.getElementById('bgMusic');
-const button = document.getElementById('musicBtn');
-
-button.addEventListener('click', () => {
-    music.play();
-    button.innerHTML = '🎵 Music Playing';
-});
